9ee0867f05e7547b0c0f4a69fd2b183bbe95d7ce
resufer <66846932+resufer@users.noreply.github.com>
2021-03-08 21:59:10 +0300

    Update Navigation.jsx

--- src/Navigation/Navigation.jsx
+++ src/Navigation/Navigation.jsx
@@ -1,14 +1,18 @@
-let Navigation = () => {
-  return (
-    <>
+import React from 'react';
+
+let Navigation = ({ heightState }) => {
+  return (<>{
+    heightState ?
+      <nav><div>
+        <span>∞ MENU</span>
+      </div></nav> :
       <nav>
         <a href='/#'>HOME</a>
         <a href='/#'>ABOUT ME</a>
         <a href='/#'>PORTFOLIO</a>
         <a href='/#'>CONTACT</a>
       </nav>
-    </>
-  )
+  }</>)
 };
 
 export default Navigation;
